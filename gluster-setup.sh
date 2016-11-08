88b45473edd2537da5bf43b7a7102b1a8bf20b68
Jam Risser <jam@jamrizzi.com>
2016-11-08 02:41:40 -0600

    Added more glusterfs setup

--- gluster-setup.sh
+++ gluster-setup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # settings
-NODE_2=rancher
 NODE_1_DOMAIN=node1.example.com
 NODE_2_DOMAIN=node2.example.com
+CLIENT_IP_1=client1.example.com
+CLIENT_IP_2=client2.example.com
 VOLUME_NAME=rancher-storage
 DATA_DIRECTORY=/exports
 
 if [ $(whoami) = "root" ]; then # if run as root
 
 # gather information
-read -p "Node 2 ("$NODE_2"): " $NODE_2_NEW
-if [ $NODE_2_NEW ]; then
-    NODE_2=$NODE_2_NEW
-fi
 read -p "Volume Name ("$VOLUME_NAME"): " $VOLUME_NAME_NEW
 if [ $VOLUME_NAME_NEW ]; then
     VOLUME_NAME=$VOLUME_NAME_NEW
@@ -26,19 +23,33 @@ read -p "Node 2 Domain ("$NODE_2_DOMAIN"): " $NODE_2_DOMAIN_NEW
 if [ $NODE_2_DOMAIN_NEW ]; then
     NODE_2_DOMAIN=$NODE_2_DOMAIN_NEW
 fi
+read -p "Client IP 1 ("$CLIENT_IP_1"): " $CLIENT_IP_1_NEW
+if [ $CLIENT_IP_1_NEW ]; then
+    CLIENT_IP_1=$CLIENT_IP_1_NEW
+fi
+read -p "Client IP 2 ("$CLIENT_IP_2"): " $CLIENT_IP_2_NEW
+if [ $CLIENT_IP_2_NEW ]; then
+    CLIENT_IP_2=$CLIENT_IP_2_NEW
+fi
 read -p "Data Directory ("$DATA_DIRECTORY"): " $DATA_DIRECTORY_NEW
 if [ $DATA_DIRECTORY_NEW ]; then
     DATA_DIRECTORY=$DATA_DIRECTORY_NEW
 fi
 
 # peer with nodes
-gluster peer probe $NODE_2
+gluster peer probe $NODE_2_DOMAIN
 gluster peer status
 
 # create storage volume
-mkdir -p $DATA_DIRECTORY
 gluster volume create $VOLUME_NAME replica 2 transport tcp $NODE_1_DOMAIN:$DATA_DIRECTORY $NODE_2_DOMAIN:$DATA_DIRECTORY force
 gluster volume start $VOLUME_NAME
+
+# secure glusterfs
+gluster volume set $VOLUME_NAME server.allow-insecure on
+gluster volume stop $VOLUME_NAME
+gluster volume start $VOLUME_NAME
+service glusterfs-server stop
+service glusterfs-server start
 
 else # not run as root
     echo "this program must be run as root"
--- node.sh
+++ node.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# settings
+DATA_DIRECTORY=/exports
+
 if [ $(whoami) = "root" ]; then # if run as root
+
+# gather information
+read -p "Data Directory ("$DATA_DIRECTORY"): " $DATA_DIRECTORY_NEW
+if [ $DATA_DIRECTORY_NEW ]; then
+    DATA_DIRECTORY=$DATA_DIRECTORY_NEW
+fi
 
 # prepare system
 apt-get update -y
@@ -10,6 +19,15 @@ apt-get install -y python-software-properties
 add-apt-repository ppa:semiosis/ubuntu-glusterfs-3.4
 apt-get update -y
 apt-get install -y glusterfs-server
+
+# create data directory
+mkdir -p $DATA_DIRECTORY
+chmod -R 777 $DATA_DIRECTORY
+
+# open permissions
+echo "option rpc-auth-allow-insecure on" | tee -a /etc/glusterfs/glusterd.vol
+service glustarfs-server stop
+service glustarfs-server start
 
 else # not run as root
     echo "this program must be run as root"
