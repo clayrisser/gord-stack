d05fb8c18a61fe293d2d06142c6708bfc2ce0ecc
Jam Risser <jam@jamrizzi.com>
2016-11-09 17:07:37 -0600

    SSL encryption works

--- orchestrator.sh
+++ orchestrator.sh
@@ -51,11 +51,20 @@ apt-get install -y docker-engine
 service docker start
 docker run hello-world
 
-# letsencrypt-nginx-proxy
-docker run -d --name nginx-proxy --restart=unless-stopped \
-       -p 80:80 \
+# install nginx
+docker run -d --name nginx --restart=unless-stopped -p 80:80 -p 443:443 \
+       --name nginx-proxy \
+       -v /tmp/ssl/certs:/etc/nginx/certs:ro \
+       -v /etc/nginx/vhost.d \
+       -v /usr/share/nginx/html \
        -v /var/run/docker.sock:/tmp/docker.sock:ro \
        jwilder/nginx-proxy
+
+docker run -d --restart=unless-stopped \
+       -v /tmp/ssl/certs:/etc/nginx/certs:rw \
+       --volumes-from nginx-proxy \
+       -v /var/run/docker.sock:/var/run/docker.sock:ro \
+       jrcs/letsencrypt-nginx-proxy-companion
 
 # install mariadb
 docker run -d --name rancherdb --restart=unless-stopped \
@@ -66,23 +75,27 @@ docker run -d --name rancherdb --restart=unless-stopped \
 
 # install rancher
 docker run -d --restart=unless-stopped --link rancherdb:mysql \
-       -p 8080:8080 \
        -e CATTLE_DB_CATTLE_MYSQL_HOST=$MYSQL_PORT_3306_TCP_ADDR \
        -e CATTLE_DB_CATTLE_MYSQL_PORT=3306 \
        -e CATTLE_DB_CATTLE_MYSQL_NAME=$RANCHER_MYSQL_DATABASE \
        -e CATTLE_DB_CATTLE_USERNAME=root \
        -e CATTLE_DB_CATTLE_PASSWORD=$MYSQL_PASSWORD \
        -e VIRTUAL_HOST=$RANCHER_DOMAIN \
+       -e VIRTUAL_PORT=8080 \
+       -e LETSENCRYPT_HOST=$RANCHER_DOMAIN \
+       -e LETSENCRYPT_EMAIL=$EMAIL \
        rancher/server:latest
 
 # install duplicati
 docker run -d --restart=unless-stopped \
-       -p 8200:8200 \
        -v /root/.config/Duplicati:/root/.config/Duplicati \
        -v /var/lib/mysql:/var/lib/mysql \
        -e DUPLICATI_PASS=$DUPLICATI_PASSWORD \
        -e MONO_EXTERNAL_ENCODINGS=UTF-8 \
        -e VIRTUAL_HOST=$DUPLICATI_DOMAIN \
+       -e VIRTUAL_PORT=8200 \
+       -e LETSENCRYPT_HOST=$DUPLICATI_DOMAIN \
+       -e LETSENCRYPT_EMAIL=$EMAIL \
        intersoftlab/duplicati:canary
 
 else # not run as root
