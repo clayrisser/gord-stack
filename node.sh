969a5ff4190e30d7343b4944c1d2123e8fb77ac9
Jam Risser <jam@jamrizzi.com>
2016-11-09 19:13:11 -0600

    Fixed node installation bugs

--- node.sh
+++ node.sh
@@ -21,21 +21,20 @@ apt-get update -y
 
 # install glusterfs
 apt-get install -y python-software-properties
-add-apt-repository ppa:semiosis/ubuntu-glusterfs-3.4
 apt-get update -y
 apt-get install -y glusterfs-server
 
 # mount and create data directory
 mkfs.xfs -i size=512 $VOLUME_MOUNT
 mkdir -p $DATA_DIRECTORY
-echo '$VOLUME_MOUNT $DATA_DIRECTORY xfs defaults 1 2' >> /etc/fstab
+echo "$VOLUME_MOUNT $DATA_DIRECTORY xfs defaults 1 2" >> /etc/fstab
 mount -a && mount
 chmod -R 777 $DATA_DIRECTORY
 
 # open permissions
 sed -i "s/option transport.socket.read-fail-log off/option transport.socket.read-fail-log off\n    option rpc-auth-allow-insecure on/g" /etc/glusterfs/glusterd.vol
-service glustarfs-server stop
-service glustarfs-server start
+service glusterfs-server stop
+service glusterfs-server start
 
 else # not run as root
     echo "this program must be run as root"
